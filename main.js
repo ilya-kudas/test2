d39913d678df273b99bdf37ea19c5bb2fd580c99
Ilya Kudas <ilya-kudas@ukr.net>
2015-02-23 15:37:28 +0200

    v3

---
+++ main.js
@@ -0,0 +1,20 @@
+var harvester = require('harvester');
+var builder = require('builder');
+var guard = require('guard');
+require('spawner');
+
+for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+
+    if (creep.memory.role == 'harvester') {
+        harvester(creep);
+    }
+
+    if (creep.memory.role == 'builder') {
+        builder(creep);
+    }
+
+    if (creep.memory.role == 'guard') {
+        guard(creep);
+    }
+}
