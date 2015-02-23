864c1264060d8b4564d2eaf1a737e201e914f31f
Ilya Kudas <ilya-kudas@ukr.net>
2015-02-23 17:46:02 +0200

    fix spawn

--- spawner.js
+++ spawner.js
@@ -1,21 +1,20 @@
 var spawn = Game.spawns.Spawn1;
 
-if(spawn.spawning != null) return;
+if (spawn.spawning == null) {
 
-var room = spawn.room;
-var harvesters = _.filter(Game.creeps, { memory: { role: 'harvester' } });
-var guards = _.filter(Game.creeps, { memory: { role: 'guard' } });
-var hostiles = room.find(Game.HOSTILE_CREEPS);
+    var room = spawn.room;
+    var harvesters = _.filter(Game.creeps, { memory: { role: 'harvester' } });
+    var guards = _.filter(Game.creeps, { memory: { role: 'guard' } });
+    var hostiles = room.find(Game.HOSTILE_CREEPS);
 
-if (hostiles.length > 0)
-{
-    if (guards.length == 2)
-        spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.HEAL], null, { role: 'guard' });
-    else
-        spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK], null, { role: 'guard' });
-}
+    if (hostiles.length > 0) {
+        if (guards.length == 2)
+            spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.HEAL], null, { role: 'guard' });
+        else
+            spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK], null, { role: 'guard' });
+    }
 
-else if (harvesters.length < 3)
-{
-    spawn.createCreep([Game.WORK, Game.CARRY, Game.MOVE], null, { role: 'harvester' });
+    else if (harvesters.length < 3) {
+        spawn.createCreep([Game.WORK, Game.CARRY, Game.MOVE], null, { role: 'harvester' });
+    }
 }
