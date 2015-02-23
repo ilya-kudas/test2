80c467087aa73b0d50846a8558fca68c3d66071c
Ilya Kudas <ilya-kudas@ukr.net>
2015-02-23 16:38:19 +0200

    v7

--- spawner.js
+++ spawner.js
@@ -1,8 +1,13 @@
 var spawn = Game.spawns.Spawn1;
-var harvesters = _.filter(Game.creeps, {
-    memory: { role: 'harvester' }
-});
+var harvesters = _.filter(Game.creeps, { memory: { role: 'harvester' } });
+var hostiles = creep.room.find(Game.HOSTILE_CREEPS);
 
-if (spawn.spawning == null && harvesters.length < 3) {
+if (hostiles.length > 0)
+{
+    spawn.createCreep([Game.ATTACK, Game.ATTACK, Game.MOVE], null, { role: 'guard' });
+}
+
+else if (spawn.spawning == null && harvesters.length < 3)
+{
     spawn.createCreep([Game.WORK, Game.CARRY, Game.MOVE], null, { role: 'harvester' });
 }
