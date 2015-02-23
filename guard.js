ad56545cf18adcc7f4894c4abea2e213d89832b4
Ilya Kudas <ilya-kudas@ukr.net>
2015-02-23 17:13:10 +0200

    ranged 1

--- guard.js
+++ guard.js
@@ -2,6 +2,6 @@ module.exports = function (creep) {
     var target = creep.pos.findClosest(Game.HOSTILE_CREEPS);
     if (target != null) {
         creep.moveTo(target);
-        creep.attack(target);
+        creep.rangedAttack(target);
     }
 }
