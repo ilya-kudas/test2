0b2954a946ed6dc114816ffbfb2aeebdbe51b724
Ilya Kudas <ilya-kudas@ukr.net>
2015-02-24 11:33:57 +0200

    all heal

--- spawner.js
+++ spawner.js
@@ -8,14 +8,16 @@ if (spawn.spawning == null) {
     var hostiles = room.find(Game.HOSTILE_CREEPS);
 
     if (hostiles.length > 0) {
-        if (guards.length == 2)
-            spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.HEAL], null, { role: 'guard' });
+        //if (guards.length == 2)
+        spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.HEAL], null, { role: 'guard' });
+        /*
         else if (guards.length < 7)
         {
             if (spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK], null, { role: 'guard' }) == Game.ERR_NOT_ENOUGH_ENERGY)
                 if (spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK], null, { role: 'guard' }) == Game.ERR_NOT_ENOUGH_ENERGY)
                     if (guards.length == 0) spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK], null, { role: 'guard' });
         }
+        */
     }
 
     else if (harvesters.length < 3) {
