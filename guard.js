1ff87eb01355875ec37a165235d321f6edd7c609
Ilya Kudas <ilya-kudas@ukr.net>
2015-02-24 11:54:10 +0200

    rangedMassAttack

--- guard.js
+++ guard.js
@@ -1,7 +1,13 @@
 module.exports = function (creep) {
+    /*
     var target = creep.pos.findClosest(Game.HOSTILE_CREEPS);
     if (target != null) {
         creep.moveTo(target);
         creep.rangedAttack(target);
+    }
+    */
+    var targets = creep.pos.findInRange(Game.HOSTILE_CREEPS, 3);
+    if (targets.length > 0) {
+        creep.rangedMassAttack();
     }
 }
--- spawner.js
+++ spawner.js
@@ -7,17 +7,15 @@ if (spawn.spawning == null) {
     var guards = _.filter(Game.creeps, { memory: { role: 'guard' } });
     var hostiles = room.find(Game.HOSTILE_CREEPS);
 
-    if (hostiles.length > 0) {
-        //if (guards.length == 2)
-        spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.HEAL], null, { role: 'guard' });
-        /*
-        else if (guards.length < 7)
+    if (hostiles.length > 0 && guards.length < 7) {
+        if (guards.length == 2)
+            spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.HEAL], null, { role: 'guard' });
+        else
         {
             if (spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK], null, { role: 'guard' }) == Game.ERR_NOT_ENOUGH_ENERGY)
                 if (spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK], null, { role: 'guard' }) == Game.ERR_NOT_ENOUGH_ENERGY)
                     if (guards.length == 0) spawn.createCreep([Game.RANGED_ATTACK, Game.RANGED_ATTACK, Game.RANGED_ATTACK], null, { role: 'guard' });
         }
-        */
     }
 
     else if (harvesters.length < 3) {
