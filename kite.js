86c6d20be4a76654eb2833b884873af8d26a05aa
Ilya Kudas <ilya-kudas@ukr.net>
2015-02-26 16:43:48 +0200

    add kite module

---
+++ kite.js
@@ -0,0 +1,17 @@
+var spawn = Game.spawns.Spawn1;
+
+module.exports = function (creep) {
+    var target = creep.pos.findClosest(Game.HOSTILE_CREEPS);
+    if (target != null) {
+        var path = creep.pos.findPathTo(target);
+        if (path.length > 4)
+            creep.move(path[0].direction);
+        else if (path.length > 3);
+            // wait
+        else if (path.length > 0)
+        {
+            creep.rangedAttack(target);
+            creep.moveTo(spawn);
+        }
+    }
+}
