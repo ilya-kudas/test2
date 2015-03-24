2e6cc494918e66d51f4cc947dedb31ef782a2ab9
Ilya Kudas <ilya-kudas@ukr.net>
2015-03-24 17:15:39 +0200

    formation

--- guard.js
+++ guard.js
@@ -1,3 +1,5 @@
+var spawn = Game.spawns.Spawn1;
+
 function getFormationPoints(pos) {
     var pp = [
         [0,0],
@@ -14,23 +16,42 @@ function getFormationPoints(pos) {
     return pp.map(function (p) { return spawn.room.getPositionAt(pos.x + p[0], pos.y + p[1]); })
 }
 
-var guards = _.filter(Game.creeps, { memory: { role: 'guard' } });
-for (var name in guards) {
-    var creep = guards[name];
-
+function sumTargets(pos)
+{
     var sum = 0;
-    var targets = creep.pos.findInRange(Game.HOSTILE_CREEPS, 3);
-    for (var t in targets)
-    {
-        if (creep.pos.inRangeTo(t, 1)) {
+    var targets = pos.findInRange(Game.HOSTILE_CREEPS, 3);
+    for (var t in targets) {
+        if (pos.inRangeTo(t, 1)) {
             sum += 10;
-        } else if (creep.pos.inRangeTo(t, 2)) {
+        } else if (pos.inRangeTo(t, 2)) {
             sum += 4;
         } else {
             sum += 1;
         }
     }
+    return sum;
+}
 
+var guards = _.filter(Game.creeps, { memory: { role: 'guard' } });
+var pp = Game.flags.Flag2 ? getFormationPoints(Game.flags.Flag2.pos) : [];
+var pl = _.filter(pp, function (n) { return spawn.room.lookForAt('creep', n) == undefined; });
+for (var name in guards) {
+    var creep = guards[name];
+
+    if (Game.flags.Flag2)
+    {
+        if (pl.length > 0)
+        {
+            creep.moveTo(pl[0]);
+        }
+        else
+        {
+            creep.moveTo(Game.flags.Flag2);
+        }
+        continue;
+    }
+
+    var sum = sumTargets(creep.pos);
     if (sum > 15) {
         creep.rangedMassAttack();
     } else {
