21bbfdb5d048c27158e9ae2be4e7a3b8cdb85b84
Ilya Kudas <ilya-kudas@ukr.net>
2015-03-25 14:37:32 +0200

    healers in formation

--- healer.js
+++ healer.js
@@ -1,5 +1,16 @@
+var com = require('common');
+
+function getFormationPoints(pos) {
+    var pp = [
+        [0, -1],
+        [1, -1],
+        [-1, -1]
+    ];
+    return pp.map(function (p) { return spawn.room.getPositionAt(pos.x + p[0], pos.y + p[1]); })
+}
+
 module.exports = function (creep) {
-    var targets = creep.pos.findInRange(Game.MY_CREEPS, 6, {
+    var targets = creep.pos.findInRange(Game.MY_CREEPS, 3, {
         filter: function (object) {
             return object.hits < object.hitsMax;
         }
@@ -7,20 +18,18 @@ module.exports = function (creep) {
     if (targets.length > 0) {
         var target = targets[0];
 
+        if (creep.hits / creep.hitsMax < 0.7 || creep.pos.findInRange(Game.HOSTILE_CREEPS, 3).length > 0) 
+            creep.moveTo(spawn);
+        else
+            creep.moveTo(target);
+
         if (creep.pos.isNearTo(target)) 
             creep.heal(target);
         else 
             creep.rangedHeal(target);
-
-        if (creep.hits / creep.hitsMax < 0.7)
-            creep.moveTo(spawn);
-        else if (creep.pos.findInRange(Game.HOSTILE_CREEPS, 3).length > 0)
-            creep.moveTo(spawn);
-        else
-            creep.moveTo(target);
     }
-    else if (creep.hits / creep.hitsMax < 0.7) 
+    else if (creep.hits / creep.hitsMax < 0.7 || creep.pos.findInRange(Game.HOSTILE_CREEPS, 3).length > 0) 
         creep.moveTo(spawn);
-    else if (creep.pos.findInRange(Game.HOSTILE_CREEPS, 3).length > 0)
-        creep.moveTo(spawn);
+    else if (Game.flags.Flag2)
+        com.formation(creep, pp);
 }
