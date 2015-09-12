374302290c6e5c0494ce413f3801e8c5f75aeb65
Ilya Kudas <ilya-kudas@ukr.net>
2015-09-12 13:42:36 +0300

    fix harvester

--- harvester.js
+++ harvester.js
@@ -1,7 +1,7 @@
 var spawn = Game.spawns.Spawn1;
 function distance(n) { return Math.pow(spawn.pos.x - n.pos.x, 2) + Math.pow(spawn.pos.y - n.pos.y, 2); }
-var source = _.sortBy(spawn.room.find(Game.SOURCES), distance)[0];
-//var source = spawn.pos.findClosest(Game.SOURCES);
+//var source = _.sortBy(spawn.room.find(Game.SOURCES), distance)[0];
+var source = spawn.pos.findClosestByRange(FIND_SOURCES);
 
 module.exports = function (creep) {
     if (creep.energy > 0) {
@@ -15,7 +15,7 @@ module.exports = function (creep) {
         }
     }
 
-    if (creep.energy < creep.energyCapacity && creep.getActiveBodyparts(Game.WORK) > 0) {
+    if (creep.carry.energy < creep.carryCapacity && creep.getActiveBodyparts(WORK) > 0) {
         creep.moveTo(source);
         creep.harvest(source);
     } else {
